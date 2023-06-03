a14de1365d0fa4941e584de8c23453a946a63cf3
Alexei <warlandwargame@gmail.com>
2023-06-04 00:39:38 +0100

    materialize update1

--- dist/assets/materialize.js
+++ dist/assets/materialize.js
@@ -1,1 +1,11 @@
-(()=>{"use strict";console.log("dom file");var e,o=document.querySelector("body");console.log("index file"),o.style.background="peachpuff",(e=document.createElement("h1")).textContent="test",o.appendChild(e),console.log("mario@thereninja.co.uk")})();
+(() => {
+  "use strict";
+  console.log("dom file");
+  var e,
+    o = document.querySelector("body");
+  console.log("index file"),
+    (o.style.background = "peachpuff"),
+    ((e = document.createElement("h1")).textContent = "test"),
+    o.appendChild(e),
+    console.log("mario@thereninja.co.uk");
+})();
